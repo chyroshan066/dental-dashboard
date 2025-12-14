fb94b812a94d1efddbf4524610bcff84c1b90227
Roshan <chyroshan066@gmail.com>
2025-12-14 09:12:25 +0545

    first commit

---
+++ dashboard/src/components/utility/NucleoIcon.tsx
@@ -0,0 +1,16 @@
+import { ClassName, Name } from "@/types"
+import { memo } from "react";
+
+interface NucleoIconProps extends Name, ClassName {
+    size: string;
+}
+
+export const NucleoIcon = memo(({
+    name, size, className
+}: NucleoIconProps) => {
+    return (
+        <i className={`ni ni-${name} ${size ? `ni-${size}` : ''} ${className}`} />
+    );
+});
+
+NucleoIcon.displayName = "NucleoIcon"
