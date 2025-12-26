f04290e73bf0333bfb42231c040b47f9fc9d2f1b
Roshan <chyroshan066@gmail.com>
2025-12-26 13:44:37 +0545

    feat(dashboard): add responsive searchbar component

--- apps/dashboard/src/components/layout/NavBar/NavBar.tsx
+++ apps/dashboard/src/components/layout/NavBar/NavBar.tsx
@@ -7,7 +7,6 @@ import {
   faBell,
   faClock,
   faCog,
-  faSearch,
   faUser,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -16,6 +15,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { memo } from "react";
 import { Breadcrumb } from "./Breadcrumb";
+import { SearchBar } from "./SearchBar";
 
 const MERGED_LINKS = [...NAVLINKS, ...ACCOUNT_PAGES];
 
@@ -54,18 +54,7 @@ export const NavBar = memo(() => {
         <Breadcrumb pageName={pageName} isProfile={isProfile} />
 
         <div className="flex items-center mt-2 grow sm:mt-0 sm:mr-6 md:mr-0 lg:flex lg:basis-auto">
-          <div className="flex items-center md:ml-auto md:pr-4">
-            <div className="relative flex flex-wrap items-stretch w-full transition-all rounded-lg ease-soft">
-              <span className="text-sm ease-soft leading-5.6 absolute z-50 -ml-px flex h-full items-center whitespace-nowrap rounded-lg rounded-tr-none rounded-br-none border border-r-0 border-transparent bg-transparent py-2 px-2.5 text-center font-normal text-slate-500 transition-all">
-                <FontAwesomeIcon icon={faSearch} />
-              </span>
-              <input
-                type="text"
-                className="pl-8.75 text-sm focus:shadow-soft-primary-outline ease-soft w-1/100 leading-5.6 relative -ml-px block min-w-0 flex-auto rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 pr-3 text-gray-700 transition-all placeholder:text-gray-500 focus:border-fuchsia-300 focus:outline-none focus:transition-shadow"
-                placeholder="Type here..."
-              />
-            </div>
-          </div>
+          <SearchBar />
           <ul className="flex flex-row justify-end pl-0 mb-0 list-none md-max:w-full">
             <li className="flex items-center">
               <Link
